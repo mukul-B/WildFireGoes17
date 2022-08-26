7914a7573ab20cb761021997aae4f75ce09008f8
mbadhan <mukulbadhan@gmail.com>
2022-08-25 17:02:43 -0700

    main correction

--- main.py
+++ main.py
@@ -13,14 +13,17 @@ from CommonFunctions import prepareDir
 from CreateDataset import createDataset
 from Evaluation import evaluate
 from GlobalValues import RAD, toExecuteSiteList
+from WriteDataset import writeDataset
 
 if __name__ == '__main__':
 
     data = pd.read_csv(toExecuteSiteList)
     locations = data["Sites"]
+    product = RAD
     # pipeline run for sites mentioned in toExecuteSiteList
-    for location in locations[:1]:
+    for location in locations:
         print(location)
-        prepareDir(location, RAD)
-        createDataset(location, product_name=RAD)
-        evaluate()
+        prepareDir(location, product)
+        createDataset(location, product_name=product)
+        evaluate(location, product)
+        writeDataset(location, product)
