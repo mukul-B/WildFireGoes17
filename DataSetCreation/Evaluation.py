ad4ffd635c75ddcc27c3a9c1e9b8f806ca3202c6
mbadhan <mukulbadhan@gmail.com>
2022-09-15 18:17:28 -0700

    evaluation correction

--- DataSetCreation/Evaluation.py
+++ DataSetCreation/Evaluation.py
@@ -16,25 +16,25 @@ import numpy as np
 import xarray as xr
 from PIL import Image
 
-from GlobalValues import viirs_dir, goes_dir, compare_dir, training_dir
+from GlobalValues import viirs_dir, goes_dir, compare_dir
+
+
+# demonstrate reference_data standardization with sklearn
 
 
 # visualising GOES and VIIRS
 def viewtiff(v_file, g_file, date, save=True, compare_dir=None):
-    # import rasterio
-    #
-    # raster = rasterio.open(v_file)
-    #
-    # print(raster.crs.linear_units)
-    # exit(0)
-
     VIIRS_data = xr.open_rasterio(v_file)
     GOES_data = xr.open_rasterio(g_file)
+
     fig, ax = plt.subplots(1, 3, constrained_layout=True)
 
-    p = ax[0].imshow(VIIRS_data.variable.data[0])
-    q = ax[1].imshow(GOES_data.variable.data[0])
-    # ploting Viirs on top of GOES
+    vd = VIIRS_data.variable.data[0]
+    gd = GOES_data.variable.data[0]
+
+    p = ax[0].imshow(vd)
+    q = ax[1].imshow(gd)
+
     ax[2].imshow((GOES_data.variable.data[0]) - (VIIRS_data.variable.data[0]))
     plt.colorbar(p, shrink=0.9)
     plt.colorbar(q, shrink=0.5)
@@ -47,6 +47,7 @@ def viewtiff(v_file, g_file, date, save=True, compare_dir=None):
 
 def PSNR(pred, gt, shave_border=0):
     imdff = pred - gt
+    print(imdff)
 
     imdff = imdff.flatten()
     rmse = math.sqrt(np.mean(np.array(imdff ** 2)))
@@ -73,5 +74,6 @@ def evaluate(location, product):
     viirs_list = os.listdir(viirs_tif_dir)
     for v_file in viirs_list:
         g_file = "GOES" + v_file[5:]
+        # print(g_file)
         # shape_check(viirs_tif_dir + v_file, goes_tif_dir + g_file)
-        viewtiff(viirs_tif_dir + v_file, goes_tif_dir + g_file, v_file[6:-4], compare_dir=comp_dir)
+        viewtiff(viirs_tif_dir + v_file, goes_tif_dir + g_file, v_file[6:-4], compare_dir=comp_dir, save=True)
