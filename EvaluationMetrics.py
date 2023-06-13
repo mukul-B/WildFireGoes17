d53aca1e4ad766e181c48b3127d3705d820bde98
mukul Badhan <mukulbadhan@nevada.unr.edu>
2023-06-12 17:45:11 -0700

    making plot function flexible for making single/ batch plot.

--- EvaluationMetrics.py
+++ EvaluationMetrics.py
@@ -21,7 +21,7 @@ VIIRS_GROUND_TRUTH = 'VIIRS Ground Truth'
 OTSU_thresholding_on_GOES = 'OTSU thresholding on GOES'
 GOES_input = 'GOES input'
 plt.style.use('plot_style/wrf')
-from GlobalValues import HC, HI, LI, LC, VIIRS_UNITS
+from GlobalValues import GOES_UNITS, HC, HI, LI, LC, VIIRS_MIN_VAL, VIIRS_UNITS
 
 
 # logging.basicConfig(filename='evaluation.log', filemode='w',format='%(message)s', level=logging.INFO)
@@ -145,11 +145,18 @@ def noralize_goes_to_radiance(ngf, gf_max, gf_min):
     return (gf_min + (ngf * ((gf_max - gf_min) / color_normal_value))).round(5)
 
 
-def noralize_viirs_to_radiance(nvf, vf_max):
+def noralize_viirs_to_radiance(nvf, vf_max,vf_min=0):
     color_normal_value = 255
-    return (nvf * (vf_max / color_normal_value)).round(2)
+    return (vf_min + (nvf * ((vf_max-vf_min) / color_normal_value))).round(2)
 
-
+class ImagePlot:
+    def __init__(self,unit,vmax,vmin,image_blocks,lable_blocks):
+        self.unit = unit
+        self.vmin = vmin
+        self.vmax = vmax
+        self.image_blocks = noralize_goes_to_radiance(image_blocks,vmax,vmin) if unit==GOES_UNITS else noralize_viirs_to_radiance(image_blocks,vmax)
+        self.lable_blocks = lable_blocks
+        
 def safe_results(prediction_rmse, prediction_IOU, input, groundTruth, path, site, gf_min, gf_max, vf_max, LOSS_NAME):
     
     iou_rmse = psnr_intersection_rmse = psnr_union_rmse = 0
@@ -215,86 +222,100 @@ def safe_results(prediction_rmse, prediction_IOU, input, groundTruth, path, site
     filename = pl[-1].split('.')
 
     # if filename[0] in ['79', '126', '199', '729', '183', '992', '140', '189', '1159', '190', '26', '188']:
-        # if filename[0] in ['78','240','249','0','6','19','2','10','14','15','27','807']:
+     # if filename[0] in ['78','240','249','0','6','19','2','10','14','15','27','807']:
     if 1:
-        # fig, axs = plt.subplots(2, 3, constrained_layout=True, figsize=(12, 8))
-        # image_blocks = ((input, th_img_i, groundTruth),
-        #                 (prediction_rmse if prediction_rmse is not None else prediction_IOU,
-        #                  th_img_rmse if prediction_rmse is not None else None,
-        #                  th3_img if prediction_IOU is not None else None))
-        image_blocks = ((noralize_goes_to_radiance(input, gf_max, gf_min),
-                         noralize_goes_to_radiance(th_img_i, gf_max, gf_min),
-                         noralize_viirs_to_radiance(groundTruth, vf_max)),
-                        (noralize_viirs_to_radiance(prediction_rmse,
-                                                    vf_max) if prediction_rmse is not None else noralize_viirs_to_radiance(
-                            prediction_IOU, vf_max),
-                         noralize_viirs_to_radiance(th_img_rmse, vf_max) if prediction_rmse is not None else None,
-                         noralize_viirs_to_radiance(th3_img, vf_max) if prediction_IOU is not None else None))
-        lable_blocks = ((GOES_input, OTSU_thresholding_on_GOES + input_dis, VIIRS_GROUND_TRUTH),
-                        (Prediction_RMSE if prediction_rmse is not None else Prediction_JACCARD,
-                         'OTSU thresholding on Prediction(RMSE)' + ret1_dis,
-                         'OTSU thresholding on Prediction(IOU)' + ret3_dis)
-                        )
-        plot_it(image_blocks,lable_blocks,condition,path)
+        g1 = ImagePlot(GOES_UNITS,gf_max, gf_min,
+                       input, 
+                       GOES_input)
+        g2 = ImagePlot(GOES_UNITS,gf_max, gf_min,
+                       th_img_i,
+                       OTSU_thresholding_on_GOES + input_dis)
+        g3 = ImagePlot(VIIRS_UNITS,vf_max,VIIRS_MIN_VAL,
+                       groundTruth,
+                       VIIRS_GROUND_TRUTH)
+        g4 = ImagePlot(VIIRS_UNITS if prediction_rmse is not None else "IOU",vf_max,VIIRS_MIN_VAL,
+                       prediction_rmse if prediction_rmse is not None else prediction_IOU,
+                       Prediction_RMSE if prediction_rmse is not None else Prediction_JACCARD)
+        g5 = ImagePlot(VIIRS_UNITS,vf_max,VIIRS_MIN_VAL,
+                       th_img_rmse if prediction_rmse is not None else None,
+                       'OTSU thresholding on Prediction(RMSE)' + ret1_dis)
+        g6 = ImagePlot(VIIRS_UNITS,vf_max,VIIRS_MIN_VAL,
+                       th3_img if prediction_IOU is not None else None,
+                       'OTSU thresholding on Prediction(IOU)' + ret3_dis)
+        # img_seq = ((g1,g2,g3),(g4,g5,g6))
+        # img_seq = ((g1,g3),(g4,g6))
+        img_seq = ((g1,g3,g4),)
+        # img_seq = ((g1,g3),)
+        plot_it(img_seq,condition,path)
         site_date_time = '_'.join(site.split('.')[1].split('_'))
         logging.info(
         f'{LOSS_NAME},{condition},{filename[0]},{str(iou_rmse) if prediction_rmse is not None else ""},{str(iou_p_iou) if prediction_IOU is not None else ""},{psnr_intersection_rmse if prediction_rmse is not None else ""},{site_date_time}')
    
     return coverage_i, iou_i, psnr_intersection_i, psnr_union_i, iou_p, psnr_intersection_p, psnr_union_p, condition
 
-def plot_it(image_blocks,lable_blocks,condition,path):
+def plot_it(img_seq,condition,path,colection=True):
     pl = path.split('/')
-    filename = pl[-1].split('.')    
-    # display = False
-    # if display:
-    for col in range(2):
-        for row in range(3):
-            if image_blocks[col][row] is not None:
-                # show_img(axs[col][row], image_blocks[col][row], lable_blocks[col][row])
-                fig2 = plt.figure()
-                ax = fig2.add_subplot()
+    filename = pl[-1].split('.')
+    c,r = len(img_seq) ,len(img_seq[0])
+    if(colection):
+        fig, axs = plt.subplots(c, r, constrained_layout=True, figsize=(12, 4))
+    for col in range(c):
+        for row in range(r):
+            
+            image_blocks=img_seq[col][row].image_blocks
+            lable_blocks=img_seq[col][row].lable_blocks
+            cb_unit=img_seq[col][row].unit
+            vmin,vmax = img_seq[col][row].vmin, img_seq[col][row].vmax
+            if(colection):
+                if(r == 1):
+                    ax = axs[col]
+                elif(c==1):
+                    ax = axs[row]
+                else:
+                    ax = axs[col][row]
+                ax.set_title(lable_blocks)
+
+            if  image_blocks is not None:
+                if(not colection):
+                    fig = plt.figure()
+                    ax = fig.add_subplot()
 
                 X, Y = np.mgrid[0:1:128j, 0:1:128j]
-                vmin = 200 if lable_blocks[col][row] in [VIIRS_GROUND_TRUTH] else None
-                cb_unit = "Background | Fire Area     " if lable_blocks[col][row] in [
-                    Prediction_JACCARD] else VIIRS_UNITS
-                if lable_blocks[col][row] in [Prediction_JACCARD]:
-                    cmap = plt.get_cmap("gray_r", 2)
-                    sc = ax.pcolormesh(Y, -X, image_blocks[col][row], cmap=cmap, vmin=vmin, vmax=420)
+                
+                # vmin = VIIRS_MIN_VAL if lable_blocks in [VIIRS_GROUND_TRUTH] else None
+                # vmax =420
+
+                # cb_unit = "Background | Fire Area     " if lable_blocks in [
+                #     Prediction_JACCARD] else VIIRS_UNITS
+                if lable_blocks in [Prediction_JACCARD]:
+                    sc = ax.pcolormesh(Y, -X, image_blocks, cmap=plt.get_cmap("gray_r", 2), vmin=vmin, vmax=vmax)
                 else:
-                    cmap = plt.get_cmap("jet")
-                    sc = ax.pcolormesh(Y, -X, image_blocks[col][row], cmap=cmap, vmin=vmin, vmax=420)
-                    cb = fig2.colorbar(sc, pad=0.01)
+                    sc = ax.pcolormesh(Y, -X, image_blocks, cmap=plt.get_cmap("jet"), vmin=vmin, vmax=vmax)
+                    cb = fig.colorbar(sc, pad=0.01,ax=ax)
                     cb.ax.tick_params(labelsize=11)
                     cb.set_label(cb_unit, fontsize=12)
-
-                plt.tick_params(left=False, right=False, labelleft=False,
-                                labelbottom=False, bottom=False)
-                # show_hist(ax, 'Histogram of GOES input', bins1, hist1 / imagesize, [ret1])
-                # plt.show()
-                # if lable_blocks[col][row] in [GOES_input, VIIRS_GROUND_TRUTH, Prediction_RMSE, Prediction_JACCARD]:
-                #     print(f'{pl[:2]}/{condition}_{filename[0]}_{lable_blocks[col][row]}.png')
-                #     loss = pl[1].split('_')[1]
-                #     if lable_blocks[col][row] in [GOES_input, VIIRS_GROUND_TRUTH]:
-                #         loss=''
-                #     plt.savefig('/'.join(pl[
-                #                          :1]) + f'/{condition}_{filename[0]}_{loss}_{lable_blocks[col][row]}.png',
-                #                 bbox_inches='tight', dpi=600)
-                #     # plt.savefig('/'.join(pl[
-                #     #                      :2]) + f'/{condition}_{filename[0]}_{lable_blocks[col][row]}.png',bbox_inches='tight',dpi=600)
-                filenamecorrection = lable_blocks[col][row].replace('\n','_').replace(' ','_').replace('.','_').replace('(','_').replace(')','_').replace(':','_')
-                plt.savefig('/'.join(pl[:2]) + f'/{condition}_{filename[0]}_{filenamecorrection}.png',
-                            bbox_inches='tight', dpi=600)
-                plt.close()
-                # plt.colorbar(p, shrink=0.9)
-                # plt.imsave('/'.join(pl[
-                #                     :2]) + f'/{condition}/{filename[0]}_{lable_blocks[col][row]}.png',
-                #            image_blocks[col][row])
-    #             , cmap='gray'
-
-    # path = '/'.join(pl[
-    #                 :2]) + f"/{condition}/{filename[0]}_{output_iou}_{psnr_intersection_i}_{psnr_union_i}_{str(round(coverage_i, 4))}.png"
-    plt.rcParams['savefig.dpi'] = 600
-    # fig.savefig(path)
-    # plt.show()
-    plt.close()
+                
+                # plt.tick_params(left=False, right=False, labelleft=False,
+                #                 labelbottom=False, bottom=False)
+                # Hide X and Y axes label marks
+                # ax.xaxis.set_tick_params(labelbottom=False)
+                # ax.yaxis.set_tick_params(labelleft=False)
+                
+                # Hide X and Y axes tick marks
+                ax.set_xticks([])
+                ax.set_yticks([])
+                if(not colection):
+                    filenamecorrection = lable_blocks.replace('\n','_').replace(' ','_').replace('.','_').replace('(','_').replace(')','_').replace(':','_')
+                    # plt.savefig('/'.join(pl[:2]) + f'/{condition}_{filename[0]}_{filenamecorrection}.png',
+                    #             bbox_inches='tight', dpi=600)
+                    plt.show()
+                    plt.close()
+    if(colection):
+        path = '/'.join(pl[
+                        :2]) + f"/{condition}/{filename[0]}.png" 
+        # path = '/'.join(pl[
+        #                 :2]) + f"/{condition}/{filename[0]}_{output_iou}_{psnr_intersection_i}_{psnr_union_i}_{str(round(coverage_i, 4))}.png"
+        plt.rcParams['savefig.dpi'] = 600
+        fig.savefig(path)
+        # plt.show()
+        plt.close()
